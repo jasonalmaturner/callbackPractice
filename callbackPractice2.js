d12b9089e98606ec53f324d06befe88137530c54
Jason Turner <jasonalmaturner@gmail.com>
2015-01-13 10:11:46 -0700

    practicing again

---
+++ callbackPractice2.js
@@ -0,0 +1,190 @@
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem 
+  //code here for sayHi
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+   
+and what you should write is the favNum function that makes the code above work, 
+    
+    
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+    
+    
+*/
+
+
+
+  //Code Here for first
+var first = function(array, cb) {
+  cb(array[0]);
+}
+  
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+first(names, function(firstName){
+  console.log('The first name in names is ', firstName)
+});
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+  //Code Here for last
+var last = function(array, cb) {
+  cb(array[array.length - 1])
+}
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+last(names, function(lastName){
+  console.log('The last name in names is ', lastName);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+
+  //Code Here for multiply
+var multiply = function(a, b, cb) {
+  cb(a * b);
+}
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ', answer); //should console.log 12
+})
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+  //Code Here for contains
+
+var contains = function (array, name, cb) {
+  var yes = false;
+  if(array.indexOf(name) !== -1) {
+    yes = true;
+  }
+  cb(yes);
+}
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+contains(names, 'Colt', function(yes){
+  console.log(yes);
+  if(yes === true){
+    console.log('Colt is in the array');
+  } else {
+    console.log('Colt is not in the array');
+  }
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for uniq
+
+var uniq = function (array, cb) {
+  var newArray = [];
+  for(var i = 0; i < array.length; i++) {
+    if(newArray.indexOf(array[i]) === -1) {
+      newArray.push(array[i]);
+    };
+  };
+  cb(newArray)
+};
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for each
+
+var each = function(array, cb) {
+  for(var i = 0; i < array.length; i++) {
+    cb(array[i], i);
+  }
+}
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+ //code here for getUserById
+
+var getUserById = function(id, cb) {
+  for(var i = 0; i < users.length; i++) {
+    if(users[i].id === id){
+      cb(users[i]);
+    }
+  }
+}
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById('16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+});
